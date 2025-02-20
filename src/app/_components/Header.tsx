e306bd499c90c575dba4e7ad041b9309967a0381
elizabethann5 <anntheresp44e@gmail.com>
2025-02-20 12:14:18 +0530

    first commit

---
+++ src/app/_components/Header.tsx
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+      <div className="flex justify-between items-center p-4 bg-gray-900 text-white shadow-md">
+        {/* Logo */}
+        <h1 className="text-xl font-bold tracking-wide">LOGO</h1>
+  
+        {/* Navigation */}
+        <ul className="flex gap-8 text-lg">
+          <li className="mr-8 cursor-pointer hover:text-green-400 transition duration-300">Log In</li>
+          <li className="cursor-pointer hover:text-green-400  transition duration-300">Sign Up</li>
+        </ul>
+      </div>
+    );
+  }
+  
+  export default Header;
---
+++ src/app/home/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        <h1 className='text-[34px] text-center text-blue-700 mb-5'>Home</h1>
+        <p className='border border-gray-300 p-5'>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+            Adipisci nobis, recusandae, suscipit aliquid rem eligendi 
+            voluptatem ratione dolorem quod et harum vel animi quasi 
+            distinctio ipsam voluptatum magni corporis culpa.
+        </p>
+    </div>
+  )
+}
+
+export default page;
